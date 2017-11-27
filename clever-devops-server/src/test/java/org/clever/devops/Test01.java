4ce8517b38f1c39cc18a401abf680e0bcc233d2c
Lzw2016 <lizhiwei1993>
2017-11-27 12:36:45 +0800

    init

---
+++ clever-devops-server/src/test/java/org/clever/devops/Test01.java
@@ -0,0 +1,35 @@
+package org.clever.devops;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.model.Info;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.DockerClientConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.clever.common.utils.mapper.JacksonMapper;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * 作者：lizw <br/>
+ * 创建时间：2017/11/27 11:40 <br/>
+ */
+@Slf4j
+public class Test01 {
+
+    private static final String dockerHost = "tcp://10.255.8.215:2375";
+    private static final String version = "1.33";
+
+    @Test
+    public void test01() throws IOException {
+        DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
+                .withDockerHost(dockerHost)
+//                .withApiVersion(version)
+                .build();
+        DockerClient dockerClient = DockerClientBuilder.getInstance(config).build();
+        Info info = dockerClient.infoCmd().exec();
+        log.info(JacksonMapper.nonEmptyMapper().toJson(info));
+        dockerClient.close();
+    }
+}
