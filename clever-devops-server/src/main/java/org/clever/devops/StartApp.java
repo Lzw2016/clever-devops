97ecb68152f2406c7f0623a8128d7ba52e3b10d6
lizw <1183409807@qq.com>
2018-03-08 13:30:37 +0800

    优化

--- clever-devops-server/src/main/java/org/clever/devops/StartApp.java
+++ clever-devops-server/src/main/java/org/clever/devops/StartApp.java
@@ -1,7 +1,6 @@
 package org.clever.devops;
 
 import lombok.extern.slf4j.Slf4j;
-import org.clever.common.utils.spring.SpringContextHolder;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,11 +17,10 @@ import java.util.TimeZone;
 @EnableTransactionManagement
 @MapperScan(basePackages = "org.clever.devops.mapper")
 @SpringBootApplication(scanBasePackages = {"org.clever"})
-public class DevopsStartApp {
+public class StartApp {
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
-        ApplicationContext ctx = SpringApplication.run(DevopsStartApp.class, args);
-        SpringContextHolder.setApplicationContext(ctx);
-        log.info("### 服务启动完成 === " + SpringContextHolder.getApplicationContext());
+        ApplicationContext ctx = SpringApplication.run(StartApp.class, args);
+        log.info("### 服务启动完成 === " + ctx);
     }
 }
